1162804cad3dbc0dd166b7e7250c5dc61c401c89
Akai <test@test.com>
2024-03-10 17:14:11 +0330

    added simple cache to balance endpoint for 5 minutes #none

--- app/routes/api/balance.go
+++ app/routes/api/balance.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/gin-gonic/gin"
 	"github.com/uussoop/idp-go/pkg/blocks"
+	"github.com/uussoop/idp-go/pkg/cache"
 	"github.com/uussoop/idp-go/pkg/jwt"
 )
 
@@ -27,6 +29,13 @@ func GetBalanceHandler(c *gin.Context) {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
 		return
 	}
+	getCache := cache.GetCache()
+	if balance, ok := getCache.Get(fmt.Sprintf("balance_get%s", username)); ok {
+
+		response.Balance = fmt.Sprintf("%f", balance)
+		c.JSON(http.StatusOK, response)
+		return
+	}
 	response.Address = username
 	balance, err := blocks.BscContract.GetTokenBalance(common.HexToAddress(username))
 	if err != nil {
@@ -35,6 +44,7 @@ func GetBalanceHandler(c *gin.Context) {
 	} else {
 
 		response.Balance = fmt.Sprintf("%f", balance)
+		getCache.Set(fmt.Sprintf("balance_get%s", username), fmt.Sprintf("%f", balance), 5*time.Minute)
 	}
 	if ok {
 		c.JSON(http.StatusOK, response)
