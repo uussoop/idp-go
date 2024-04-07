d4c0c242e7c6ac5be7fdcda01e54da03a52a91ce
akai <test@example.com>
2024-04-07 09:34:45 -0400

    update ratelimit to send xlimit header #none

--- app/middleware/ratelimit/limit.go
+++ app/middleware/ratelimit/limit.go
@@ -2,12 +2,21 @@ package ratelimit
 
 import (
 	"fmt"
+	"strconv"
 
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/uussoop/idp-go/pkg/cache"
 )
+
+func calculateMaxRequestsPerHour(maxRequest int, duration time.Duration) string {
+	// Calculate the number of requests per hour
+	requestsPerHour := float64(maxRequest) / duration.Hours()
+
+	// Convert the result to a string
+	return fmt.Sprintf("%d ", int(requestsPerHour))
+}
 
 func RateLimit(_key string, maxRequest int, duration time.Duration) gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -17,11 +26,18 @@ func RateLimit(_key string, maxRequest int, duration time.Duration) gin.HandlerF
 		key := fmt.Sprintf("rl:%s:%s", fingerPrint, _key)
 
 		getCache := cache.GetCache()
-		data, _, ok := getCache.GetWithExpiration(key)
+		data, exptime, ok := getCache.GetWithExpiration(key)
 		reqNum := 0
 
 		if ok {
 			reqNum = data.(int)
+			c.Writer.Header().Set("X-RateLimit-Limit", calculateMaxRequestsPerHour(maxRequest, duration))
+			c.Writer.Header().Set("X-RateLimit-Remaining", fmt.Sprintf("%d ", int(maxRequest-reqNum)))
+			c.Writer.Header().Set("X-RateLimit-Reset", strconv.FormatInt(exptime.Unix(), 10))
+		} else {
+			c.Writer.Header().Set("X-RateLimit-Limit", calculateMaxRequestsPerHour(maxRequest, duration))
+			c.Writer.Header().Set("X-RateLimit-Remaining", fmt.Sprintf("%d ", int(maxRequest)))
+			c.Writer.Header().Set("X-RateLimit-Reset", strconv.FormatInt(time.Now().Add(duration).Unix(), 10))
 		}
 
 		if ok {
